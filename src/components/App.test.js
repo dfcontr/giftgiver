55a622a21a8514a0ea9ee460287e59cc215e229e
dfcontr <daniel.contreras@outlook.com>
2017-11-29 12:03:22 -0700

    Add project to Git

---
+++ src/components/App.test.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
+
+describe('App', () => {
+  // Shallow: Only render the outer most component
+  const app = shallow(<App />);
+
+  it('renders correctly', () => {
+    expect(app).toMatchSnapshot();
+  });
+
+  it('initializes the `state` with an empty list of gifts', () => {
+    expect(app.state().gifts).toEqual([]);
+  });
+
+  describe('when clicking the `add-gift` button', () => {
+    const id = 1;
+
+    beforeEach(() => {
+      app.find('.btn-add').simulate('click');
+    });
+
+    afterEach(() => {
+      app.setState({ gifts: [] });
+    });
+
+    it('Adds a new gift to `state`', () => {
+      expect(app.state().gifts).toEqual([{ id }]);
+    });
+
+    it('Adds a new gift to the rendered list', () => {
+      expect(app.find('.gift-list').children().length).toEqual(1);
+    });
+
+    it('Creates a Gift component', () => {
+      expect(app.find('Gift').exists()).toBe(true);
+    });
+
+    describe('and the user wants to remve the added gift', () => {
+      beforeEach(() => {
+        // Get access to helper methods
+        app.instance().removeGift(id);
+      });
+
+      it('removes the gift from `state`', () => {
+        expect(app.state().gifts).toEqual([]);
+      });
+    });
+  });
+});
